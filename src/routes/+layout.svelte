dfd778987afdfd21069bd6cfa50d90eb30d4a93f
Hunter Johnston <johnstonhuntera@gmail.com>
2023-01-02 10:59:11 -0500

    dev-final

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,72 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script lang="ts">
+	import '../app.postcss';
+	import { page } from '$app/stores';
+	import { enhance, type SubmitFunction } from '$app/forms';
+
+	const submitUpdateTheme: SubmitFunction = ({ action }) => {
+		const theme = action.searchParams.get('theme');
+
+		if (theme) {
+			document.documentElement.setAttribute('data-theme', theme);
+		}
+	};
+
+	const themes = [
+		'light',
+		'dark',
+		'cupcake',
+		'bumblebee',
+		'emerald',
+		'corporate',
+		'synthwave',
+		'retro',
+		'cyberpunk',
+		'valentine',
+		'halloween',
+		'garden',
+		'forest',
+		'aqua',
+		'lofi',
+		'pastel',
+		'fantasy',
+		'wireframe',
+		'black',
+		'luxury',
+		'dracula',
+		'cmyk',
+		'autumn',
+		'business',
+		'acid',
+		'lemonade',
+		'night',
+		'coffee',
+		'winter'
+	];
+</script>
+
+<div class="bg-base-300 min-h-full">
+	<div class="navbar bg-base-100 px-6">
+		<div class="flex-1">
+			<a href="/" class="font-bold text-xl">SvelteKit Theme Switching</a>
+		</div>
+		<div class="flex-none">
+			<ul class="menu menu-horizontal px-1 z-50">
+				<li>
+					<button> Set Theme 🎨 </button>
+					<ul class="p-2 bg-base-100 w-full max-h-96 overflow-y-scroll">
+						<form method="POST" use:enhance={submitUpdateTheme} class="max-h-96">
+							{#each themes as theme}
+								<li>
+									<button formaction="/?/setTheme&theme={theme}&redirectTo={$page.url.pathname}"
+										>{theme}</button
+									>
+								</li>
+							{/each}
+						</form>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<slot />
+</div>
---
+++ src/routes/hello/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Hello</h1>
+
+<button class="btn btn-primary">Primary Button</button>
+<button class="btn btn-secondary">Secondary Button</button>
