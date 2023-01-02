78c4ab5f1e28b3d02aa98edb7181a7649517b8f9
Hunter Johnston <johnstonhuntera@gmail.com>
2023-01-02 11:01:08 -0500

    starter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,47 +1,5 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { page } from '$app/stores';
-	import { enhance, type SubmitFunction } from '$app/forms';
-
-	const submitUpdateTheme: SubmitFunction = ({ action }) => {
-		const theme = action.searchParams.get('theme');
-
-		if (theme) {
-			document.documentElement.setAttribute('data-theme', theme);
-		}
-	};
-
-	const themes = [
-		'light',
-		'dark',
-		'cupcake',
-		'bumblebee',
-		'emerald',
-		'corporate',
-		'synthwave',
-		'retro',
-		'cyberpunk',
-		'valentine',
-		'halloween',
-		'garden',
-		'forest',
-		'aqua',
-		'lofi',
-		'pastel',
-		'fantasy',
-		'wireframe',
-		'black',
-		'luxury',
-		'dracula',
-		'cmyk',
-		'autumn',
-		'business',
-		'acid',
-		'lemonade',
-		'night',
-		'coffee',
-		'winter'
-	];
 </script>
 
 <div class="bg-base-300 min-h-full">
@@ -54,15 +12,10 @@
 				<li>
 					<button> Set Theme 🎨 </button>
 					<ul class="p-2 bg-base-100 w-full max-h-96 overflow-y-scroll">
-						<form method="POST" use:enhance={submitUpdateTheme} class="max-h-96">
-							{#each themes as theme}
-								<li>
-									<button formaction="/?/setTheme&theme={theme}&redirectTo={$page.url.pathname}"
-										>{theme}</button
-									>
-								</li>
-							{/each}
-						</form>
+						<li>
+							<button>Dark</button>
+							<button>Light</button>
+						</li>
 					</ul>
 				</li>
 			</ul>
